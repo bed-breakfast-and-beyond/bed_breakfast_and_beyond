f85266616ca77dc7f453f1bf8acfb67671bfd479
Peter Wong <wong.peter.79@gmail.com>
2012-12-02 10:39:52 -0800

    Revert "Added Peters changes to the index.html and Carlos map changes."
    This reverts commit 2777ba034b31ac25eda242b573bbdf7e810cfa96.

--- DisplayMap.js
+++ DisplayMap.js
@@ -1,48 +1,34 @@
-var geocoder;
-var map;
-function displayMap(address, name) {
-    geocoder = new google.maps.Geocoder();
-    var mapOptions = {
-      zoom: 8,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    map = new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
-        
-        var center = address;
-        geocoder.geocode( { 'address': center}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-          } else {
-        alert("Geocode was not successful for the following reason: " + status);
-      }
-    });
-                
-        var addresses = [
-        address
-        ];
-                
-        var infowindow = new google.maps.InfoWindow();
-        var marker,i;
-        for (i = 0; i < addresses.length; i++) {
-                (function(address) {
-                geocoder.geocode( {'address': address}, function(results, status) {
-                        if (status == google.maps.GeocoderStatus.OK) {
-                                 marker = new google.maps.Marker({
-                                        map: map,
-                                        position: results[0].geometry.location
-                                });
-
-                        } 
-                        else{
-                                alert("Geocode was not successful for the following reason: " + status);
-                }
-                                google.maps.event.addListener(marker, 'click', function() {
-                        infowindow.setContent(name);
-                        infowindow.open(map, marker);
-                        });
-    });
-                })(addresses[i]);
-  }
- 
-        
-}
+	function displayMap(listings, location){
+	
+		var geocoder = new GClientGeocoder();
+		var map = new GMap2(document.getElementById("map_canvas"));
+		var i;
+		/*
+		var postcodes = new Array();
+		for(i=0; i < listings.length; i++){
+			postcodes[i] = listings[i].address;
+		}
+		*/
+		geocoder.getLatLng(location , function (point) {
+		map.setCenter(point, 9);
+		map.addOverlay(new GMarker(point));
+		});
+		
+		/*
+		geocoder.getLatLng(location, function (point) {         
+				if (point) {
+					
+				}
+			});
+		
+		/*
+		for (i = 0; i < postcodes.length; i++) {
+			geocoder.getLatLng(postcodes[i], function (point) {         
+				if (point) {
+					map.addOverlay(new GMarker(point));
+				}
+			});
+		}
+		*/
+		
+	}
